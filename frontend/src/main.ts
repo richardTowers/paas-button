13876113835d19d1db35628193843359b81b2cb3
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-09-28 15:36:54 +0100

    Add basic govuk-styles

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,17 @@
 import express from 'express'
+import nunjucks from 'nunjucks'
+import path from 'path'
+
+const port = process.env['PORT'] || 8080
 
 const app = express()
+nunjucks.configure(['src/', 'node_modules/govuk-frontend'], {express: app})
 
-app.all('*', (req, res, next) => {
-  res.end('Hello world!')
+app.use('/assets', express.static(path.join(__dirname, '../node_modules/govuk-frontend/assets')))
+app.use('/styles.css', express.static(path.join(__dirname, 'styles.css')))
+
+app.get('/', (req, res, next) => {
+  res.render('components/deploy/deploy.njk')
 })
-app.listen(8080)
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
