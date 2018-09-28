d44f37d8a751475c8709dc1d895e62534e222c79
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-09-28 16:00:33 +0100

    Add very basic views

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -10,8 +10,7 @@ nunjucks.configure(['src/', 'node_modules/govuk-frontend'], {express: app})
 app.use('/assets', express.static(path.join(__dirname, '../node_modules/govuk-frontend/assets')))
 app.use('/styles.css', express.static(path.join(__dirname, 'styles.css')))
 
-app.get('/', (req, res, next) => {
-  res.render('components/deploy/deploy.njk')
-})
+app.get('/', (_, res) => res.render('components/home/home.njk'))
+app.get('/deploy', (_, res) => res.render('components/deploy/deploy.njk'))
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
