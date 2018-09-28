f727b6e53b213ae081e160f69ea9d18785720b78
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-09-28 17:20:54 +0100

    Handle referer / query params more carefully
    Also talk to github. Will need to API token this at some point.

---
+++ frontend/src/lib/getGithubRepoToDeploy.ts
@@ -0,0 +1,17 @@
+/**
+ * Sanitizes raw user input into a supported URL (GitHub only for now)
+ * 
+ * @param rawInput raw user input (either from the referer header or the path)
+ * @returns a string of the form "organisation/repository"
+ */
+export default function getGithubRepoToDeploy(rawInput: string): {owner:string,repo:string}|null {
+  const pattern = /(?:https?:\/\/)?(?:github.com)?\/?([^\/]+)\/([^\/]+)/
+  const result = pattern.exec(rawInput)
+  if (result === null) {
+    return null
+  }
+  return {
+    owner: result[1],
+    repo: result[2],
+  }
+}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import nunjucks from 'nunjucks'
 import path from 'path'
+import octokit from '@octokit/rest'
+import getGithubRepoToDeploy from './lib/getGithubRepoToDeploy'
 
 const port = process.env['PORT'] || 8080
+const githubClient = new octokit();
 
 const app = express()
 nunjucks.configure(['src/', 'node_modules/govuk-frontend'], {express: app})
@@ -11,13 +14,20 @@ app.use('/assets', express.static(path.join(__dirname, '../node_modules/govuk-fr
 app.use('/styles.css', express.static(path.join(__dirname, 'styles.css')))
 
 app.get('/', (_, res) => res.render('components/home/home.njk'))
-app.get('/deploy', (req, res) => {
-  // TODO: extract this into a nice testable function
-  const urlToDeploy = req.query.url || req.headers['referer']
-  if (!urlToDeploy) {
-    res.redirect('/?error=no-url')
+app.get('/deploy', async (req, res) => {
+  const githubRepo = getGithubRepoToDeploy(req.query.url || req.headers['referer'])
+  if (!githubRepo) {
+    return res.redirect('/?error=no-url')
   }
-  res.render('components/deploy/deploy.njk', { urlToDeploy })
+
+  const repo = await githubClient.repos.get(githubRepo)
+  if (repo.status === 200) {
+    const description = repo.data.description
+    return res.render('components/deploy/deploy.njk', { urlToDeploy: `${githubRepo.owner}/${githubRepo.repo}`, description })
+  } else {
+    return res.redirect('/?error=repo-not-found')
+  }
+
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
