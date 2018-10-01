2328a00045de39ddda1059924c1b5aaad4c35c54
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-10-01 09:49:00 +0100

    WIP: Wire up authentication with UAA

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,11 +2,36 @@ import express from 'express'
 import nunjucks from 'nunjucks'
 import path from 'path'
 import deploy from './components/deploy/deploy'
+import passport from 'passport'
+import OAuth2Strategy from 'passport-oauth2'
 
 const port = process.env['PORT'] || 8080
 
 const app = express()
 nunjucks.configure(['src/', 'node_modules/govuk-frontend'], {express: app})
+
+// TODO error if this isn't set instead of defaulting it
+const cloudFoundryAPI = process.env['API_URL'] || 'https://api.towers.dev.cloudpipeline.digital'
+
+// TODO need a cloud foundry client here... copy the one from paas-admin??
+
+
+const options = {
+  authorizationURL: 'TODO',
+  clientID:         'TODO',
+  clientSecret:     'TODO',
+  tokenURL:         'TODO',
+}
+passport.use(new OAuth2Strategy(
+  options,
+  (accessToken: string, refreshToken: string, profile: any, cb: any) => cb(null, {accessToken, refreshToken, profile}))
+)
+app.use(passport.initialize())
+app.use(passport.session())
+passport.serializeUser((user: {accessToken: string}, cb: (err: any, id?: string) => void) => cb(null, user.accessToken))
+passport.deserializeUser((accessToken: string, cb: (err: any, user?: {accessToken: string}) => void) => cb(null, {accessToken}))
+app.get('/auth/login', passport.authenticate('oauth2'))
+app.get('/auth/login/callback', passport.authenticate('oauth2', {}), (_req, res) => res.redirect('/'))
 
 app.use('/assets', express.static(path.join(__dirname, '../node_modules/govuk-frontend/assets')))
 app.use('/styles.css', express.static(path.join(__dirname, 'styles.css')))
