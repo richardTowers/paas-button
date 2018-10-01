850353468180c46566b5782fd01d09b1b1ad66be
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-10-01 08:22:22 +0100

    Extract "deploy" component

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,9 @@
 import express from 'express'
 import nunjucks from 'nunjucks'
 import path from 'path'
-import octokit from '@octokit/rest'
-import getGithubRepoToDeploy from './lib/getGithubRepoToDeploy'
+import deploy from './components/deploy/deploy'
 
 const port = process.env['PORT'] || 8080
-const githubClient = new octokit();
 
 const app = express()
 nunjucks.configure(['src/', 'node_modules/govuk-frontend'], {express: app})
@@ -14,20 +12,6 @@ app.use('/assets', express.static(path.join(__dirname, '../node_modules/govuk-fr
 app.use('/styles.css', express.static(path.join(__dirname, 'styles.css')))
 
 app.get('/', (_, res) => res.render('components/home/home.njk'))
-app.get('/deploy', async (req, res) => {
-  const githubRepo = getGithubRepoToDeploy(req.query.url || req.headers['referer'])
-  if (!githubRepo) {
-    return res.redirect('/?error=no-url')
-  }
-
-  const repo = await githubClient.repos.get(githubRepo)
-  if (repo.status === 200) {
-    const description = repo.data.description
-    return res.render('components/deploy/deploy.njk', { urlToDeploy: `${githubRepo.owner}/${githubRepo.repo}`, description })
-  } else {
-    return res.redirect('/?error=repo-not-found')
-  }
-
-})
+app.get('/deploy', deploy)
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
