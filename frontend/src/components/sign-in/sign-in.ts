740d772dfa40cadf8ab71029109c24704b5a40c0
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-10-03 14:52:42 +0100

    Nicer sign in page

--- frontend/src/components/sign-in/sign-in.ts
+++ frontend/src/components/sign-in/sign-in.ts
@@ -1,5 +1,9 @@
 import { Request, Response, NextFunction } from 'express'
     
 export default async function signIn(req: Request, res: Response, next: NextFunction): Promise<any> {
-  res.render('components/sign-in/sign-in.njk')
+  if (!req.session) { throw new Error('Session required') }
+  const githubRepo = req.session['githubRepo']
+  if (!githubRepo) { throw new Error('Expected a github repo') }
+  
+  res.render('components/sign-in/sign-in.njk', {githubRepo})
 }
