e30144e721289b8e9a467b5dc6f1a6c55ddd5906
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-10-03 13:39:51 +0100

    Don't hardcode the redirect url

--- frontend/src/components/deploy-succeeded/deploy-succeeded.ts
+++ frontend/src/components/deploy-succeeded/deploy-succeeded.ts
@@ -1,6 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 
 export default async function deploySucceeded(req: Request, res: Response, next: NextFunction): Promise<any> {
-  res.render('components/deploy-succeeded/deploy-succeeded.njk')
+  if (!req.session) { throw new Error('Session required') }
+  if (!req.user) { throw new Error('User required') }
+
+  const githubRepo: {owner: string, repo: string} = req.session['githubRepo']
+  if (!githubRepo) { throw new Error('Session should contain a github repo') }
+
+  // TODO: This won't be correct if the user specifies a name
+  const url = `https://${githubRepo.repo}.towers.dev.cloudpipelineapps.digital`
+  res.render('components/deploy-succeeded/deploy-succeeded.njk', {url})
 }
 
